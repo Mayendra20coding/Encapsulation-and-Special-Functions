d373928a06a7f088a34ed82bd9a6f4690ba8c73e
Mayendra20coding <bandara.mayendra@gmail.com>
2025-08-14 20:21:49 +1200

    keepitprivate

---
+++ keepitprivate.py
@@ -0,0 +1,9 @@
+class myClass:
+        __privateVar = 27;
+        def __privateMeth(self):
+                print('I am inside class myClass')
+        def hello(self):
+                        print('Private Variable Value:', myClass.__privateVar)
+foo = myClass()
+foo.hello()
+foo.__privateMeth
